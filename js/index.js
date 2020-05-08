8da2c6c01601adda7acb6a0edbc8b88b64887b5d
joaorceschini <jrvceschini@gmail.com>
2020-05-08 02:10:10 -0300

    Páginas concluidas e erros consertados

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 const body = document.querySelector('body');
 const btnOpen = document.getElementById('btn-open');
 const closeMenu = document.querySelectorAll('[data-close="menu"]');
+const btnFooter = document.querySelector('#footer button');
+
+btnFooter.addEventListener("click", e => {
+    window.scrollTo(0, 0)
+})
 
 btnOpen.addEventListener('click', e => {
     body.classList.add('open-menu');
@@ -11,3 +16,10 @@ closeMenu.forEach( el => {
         body.classList.remove('open-menu');
     });
 });
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 44.531274, lng: 10.868766 },
+        zoom: 14
+    });
+}
